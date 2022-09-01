8261b118138230e7d1c2ac58e57377836c3e2c8b
Daniel Ratzlaff <Asylumgames39@gmail.com>
2022-09-01 15:28:06 -0500

    fixed deprecation. finished dynamic list

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  const users = [
+    { name: "Randy Cox", id: 1 },
+    { name: "Cody Thaller", id: 2 },
+    { name: "Michelle Hoelzer", id: 3 },
+  ];
+
+  return (
+    <div>
+      <h1>User names</h1>
+      <ul>
+        {users.map(user => (
+          <li>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
